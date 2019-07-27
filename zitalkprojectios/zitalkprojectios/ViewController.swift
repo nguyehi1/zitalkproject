f4bd7153b3415bba99844468d547f75f7065da0d
Hien Nguyen <hiennguyen@Hiens-MacBook-Pro.local>
2019-07-26 22:08:35 -0700

    installed iOS pod

---
+++ zitalkprojectios/zitalkprojectios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zitalkprojectios
+//
+//  Created by Hien Nguyen on 7/25/19.
+//  Copyright © 2019 Hien Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
