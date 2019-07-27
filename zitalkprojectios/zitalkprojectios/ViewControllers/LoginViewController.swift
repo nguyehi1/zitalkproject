2c01ce2f0c4e0ccfd471fa6b840703c120fcbabc
Hien Nguyen <hiennguyen@Hiens-MacBook-Pro.local>
2019-07-26 22:46:54 -0700

    implement Login UI in Main.storyboard

---
+++ zitalkprojectios/zitalkprojectios/ViewControllers/LoginViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Alamofire
+
+class LoginViewController: UIViewController, UITextFildDelegate {
+    @IBoutlet var usernameTextField:UITextField!
+    @IBoutlet var passwordTextField:UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad ()
+    }
+ }
