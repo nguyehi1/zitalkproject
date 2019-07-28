1c5facb624bf6cf56e0dab181b99d418cc712993
Hien Nguyen <hiennguyen@Hiens-MacBook-Pro.local>
2019-07-28 13:25:45 -0700

    addressed constraints ato make the UI shown

--- zitalkprojectios/zitalkprojectios/ViewController.swift
+++ zitalkprojectios/zitalkprojectios/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func showMessage(sender:UIButton) {
+    @IBAction func showMessage(sender: UIButton) {
         let alertController = UIAlertController(title: "Welcome to ZiTalk", message: "Zitalk Adventure", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil))
