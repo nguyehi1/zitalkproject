8199eb345a8abd25b6fdbb2863310d4711d93304
Hien Nguyen <hiennguyen@Hiens-MacBook-Pro.local>
2019-07-27 21:49:51 -0700

    modified LoginViewController

--- zitalkprojectios/zitalkprojectios/ViewController.swift
+++ zitalkprojectios/zitalkprojectios/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage(sender:UIButton) {
+        let alertController = UIAlertController(title: "Welcome to ZiTalk", message: "Zitalk Adventure", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
