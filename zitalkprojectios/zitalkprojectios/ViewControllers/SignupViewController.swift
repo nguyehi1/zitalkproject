9a190ff83a0a6eb58bfe0cb7987f7a19fcec6ff1
Hien Nguyen <hiennguyen@Hiens-MacBook-Pro.local>
2019-07-28 13:49:12 -0700

    updated LoginViewController

---
+++ zitalkprojectios/zitalkprojectios/ViewControllers/SignupViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Alamofire
+
+class SignupViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var usernameTextField:UITextField!
+    @UIOutlet var passwordTextField:UITextField!
+
+    override func viewDidload() {
+        super.viewDidload()
+    }
+}
